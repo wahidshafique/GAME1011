b653b165b62531642f10e476b6f383462a451163
Wahid Shafique <wahidshafique@hotmail.com>
2015-01-19 14:46:51 -0500

    minor update

--- GAME1011_A1_ShafiqueWahid/GAME1011_A1_ShafiqueWahid/Source.cpp
+++ GAME1011_A1_ShafiqueWahid/GAME1011_A1_ShafiqueWahid/Source.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 void process();
+void kilogramsToPounds();
+void celsiusToKelvin();
+void teaspoonsToMil();
+void Multiplication();
 
 void write(string input){
 	cout << input << endl;
@@ -23,6 +27,20 @@ void clearScreen() {
 	cout << string(100, '\n'); //because system() is evil
 }
 
+void endMessage(void(*function)()){
+	write("");
+	write("press 6 to exit to menu or 7 to run this again ");
+	int input;
+	cin >> input;
+	if (input == 7){
+		clearScreen();
+		(*function)();
+	} else {
+		clearScreen();
+		menu();
+	}
+}
+
 void kilogramsToPounds(){
 	write("Kilograms to Pounds");
 	write("-------------------");
@@ -31,44 +49,57 @@ void kilogramsToPounds(){
 	int kilograms;
 	cin >> kilograms;
 	write("");
-	cout << kilograms << " Kilograms = " << kilograms*2.2 << " Pounds" << endl;
-	write("");
+	cout << kilograms << " Kilograms = " << kilograms * 2.2 << " Pounds" << endl;
 
-	write("press 6 to exit to menu or 7 to run this again ");
-	int input;
-	cin >> input; 
-	if (input == 7){
-		clearScreen();
-		kilogramsToPounds();
-	}
-	else {
-		clearScreen();
-		menu(); }
+	endMessage(&kilogramsToPounds);
 }
 
 void celsiusToKelvin(){
-	write("Kilograms to Pounds");
+	write("Celsius to Kelvin");
 	write("-------------------");
 	write("");
-	cout << "Enter Kilograms: " << flush;
-	int kilograms;
-	cin >> kilograms;
+	cout << "Enter Celsius: " << flush;
+	int celsius;
+	cin >> celsius;
 	write("");
-	cout << kilograms << " Kilograms = " << kilograms*2.2 << " Pounds" << endl;
-	write("");
+	cout << celsius << " Celsius = " << celsius + 273.15 << " Kelvin" << endl;
 
-	write("press 6 to exit to menu or 7 to run this again ");
-	int input;
-	cin >> input;
-	if (input == 7){
-		clearScreen();
-		celsiusToKelvin();
-	}
-	else {
-		clearScreen();
-		menu();
-	}
+	endMessage(&celsiusToKelvin);
 }
+
+void teaspoonsToMil(){
+	write("Teaspoons to Milliliters");
+	write("-------------------");
+	write("");
+	cout << "Teaspoons: " << flush;
+	int teaspoons;
+	cin >> teaspoons;
+	write("");
+	cout << teaspoons << " Teaspoons = " << teaspoons * 4.92 << " Milliliters" << endl;
+
+	endMessage(&teaspoonsToMil);
+}
+
+void Multiplication(){
+	write("Multiplication Table");
+	write("-------------------");
+	write("");
+	cout << "Enter in a value to make the table : " << flush;
+	int number;
+	cin >> number;
+	write("");
+	if (number > 0){
+		for (int i = 1; i < number; i++){
+			for (int j = 1; j < number; j++){
+				cout << i*number << endl;
+				cout << j*number << endl;
+			}
+		}
+
+	}
+	else Multiplication();
+}
+
 
 void process(){
 	int input;
@@ -81,12 +112,15 @@ void process(){
 		break;
 	case 2:
 		clearScreen();
+		celsiusToKelvin();
 		break;
 	case 3:
 		clearScreen();
+		teaspoonsToMil();
 		break;
 	case 4:
 		clearScreen();
+		Multiplication();
 		break;
 	case 5:
 		clearScreen();
